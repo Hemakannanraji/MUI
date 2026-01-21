18e86fff7483d2d84544ad065fe3a355ff625961
Hemakannanraji <hema2005kannan@gmail.com>
2026-01-21 10:05:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+// import BasicButtons from './ui/BasicButton';
+// import CheckboxesGroup from './ui/CheckboxesGroup';
+// import { Container } from '@mui/material';
+// import IconAvatars from './ui/IconAvatars';
+// import ContinuousSlider from './ui/ContinuousSlider';
+// import MenuAppBar from './ui/MenuAppBar';
+// import Elevation from './ui/Elevation';
+import ButtonAppBar from './components/ButtonAppBar';
+import BasicTable from './components/BasicTable';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Container>
+    //   <MenuAppBar />
+    //   <BasicButtons />
+    //   <CheckboxesGroup />
+    //   <IconAvatars />
+    //   <ContinuousSlider />
+    //   <Elevation />
+    //</Container>
+    <div>
+      <ButtonAppBar/>
+      <br/>
+      <BasicTable/>
     </div>
   );
 }
